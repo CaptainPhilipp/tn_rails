b91e77b62f72cae7ee5df01528348b047efc72c7
CaptainPhilipp <Ph-s@mail.ru>
2017-03-23 23:35:40 +0300

    Database config, update ruby to 2.4.0, rails to 5.0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :railway_stations
   resources :trains
+  root 'railway_stations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
