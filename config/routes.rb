9adc9a89221aac7a7aa2b70ad9c4fcd183a6caec
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-03-24 11:22:53 +0300

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   resources :trains
   # resources :routes # это имелось в виду? а то мало ли, может вручную каждый
   get    '/routes',         to: 'routes#index'
-  get    '/routes/new',     to: 'routes#new'
+  get    '/routes/new',     to: 'routes#new', as: 'new'
   post   '/routes',         to: 'routes#create'
   get    '/route/:id',      to: 'routes#show'
-  post   '/route/:id/edit', to: 'routes#edit'
+  get    '/route/:id/edit', to: 'routes#edit', as: 'edit'
   patch  '/route/:id',      to: 'routes#update'
   delete '/route/:id',      to: 'routes#destroy'
 
