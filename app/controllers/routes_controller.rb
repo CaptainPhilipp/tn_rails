742c047d7682a3c60dded9f02fd25cab018c86fd
Iamphilipp <ph-s@mail.ru>
2017-03-24 14:31:11 +0400

    controller CRUD functionality

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,4 +1,6 @@
 class RoutesController < ApplicationController
+  before_action :find_route, only: [:show, :edit, :update, :destroy]
+
   def index
     @routes = Route.all
   end
@@ -8,22 +10,33 @@ class RoutesController < ApplicationController
   end
 
   def create
-    # TODO
+    @route = Route.create(route_params)
+    render @route
   end
 
   def show
-    # TODO
   end
 
   def edit
-    # TODO
   end
 
   def update
-    # TODO
+    @route.update(route_params)
+    @route.reload
+    render :edit
   end
 
   def destroy
-    # TODO
+    @route.delete
+  end
+
+  private
+
+  def find_route
+    @route = Route.find(params[:id])
+  end
+
+  def route_params
+    params.require(:route).permit(:title)
   end
 end
