79fad9ad9c5df121984fe5b94ef0ed9fcd59d4eb
CaptainPhilipp <Ph-s@mail.ru>
2017-03-24 09:33:20 +0300

    Controller, model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :railway_stations
   resources :trains
   resources :routes # это имелось в виду? а то мало ли, может вручную каждый
+  get    '/routes',         to: 'routes#index'
+  get    '/routes/new',     to: 'routes#new'
+  post   '/routes',         to: 'routes#create'
+  get    '/route/:id',      to: 'routes#show'
+  post   '/route/:id/edit', to: 'routes#edit'
+  patch  '/route/:id',      to: 'routes#update'
+  delete '/route/:id',      to: 'routes#destroy'
 
   root 'railway_stations#index'
 
