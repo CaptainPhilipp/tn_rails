2f05ae7029b44bd8815eb5cf7c052027aed5e7aa
CaptainPhilipp <Ph-s@mail.ru>
2017-03-26 00:40:22 +0300

    Train may not to have a Route

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,5 @@
 class Train < ApplicationRecord
-  belongs_to :route
+  belongs_to :route, optional: true
   belongs_to :current_station, class_name: 'RailwayStation',
                               foreign_key: :railway_station_id
 end
