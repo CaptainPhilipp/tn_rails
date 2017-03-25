0e19a106392d56543511f88f696fd856fed57faa
CaptainPhilipp <Ph-s@mail.ru>
2017-03-25 23:55:12 +0300

    many Trains to one Route
    without foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325204550) do
+ActiveRecord::Schema.define(version: 20170325205824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170325204550) do
     t.string   "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "route_id"
   end
 
   add_foreign_key "railway_stations_routes", "railway_stations"
