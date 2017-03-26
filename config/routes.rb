4868ac42a85a494cd6d61062fc016940c852aae2
CaptainPhilipp <Ph-s@mail.ru>
2017-03-27 02:53:15 +0300

    train of chahges
    кой-что в корне изменил, когда наконец смог посмотреть скринкаст >_< и то не весь, увы.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :railway_stations
-  resources :trains do
-    put 'route/:route_id', action: 'change_route', as: 'route'
-  end
-
+  resources :trains
   resources :routes
 
   root 'railway_stations#index'
