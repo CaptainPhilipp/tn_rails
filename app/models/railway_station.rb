f2bd1519e9d3e7c35954dce40580c2f5fbf5634e
CaptainPhilipp <Ph-s@mail.ru>
2017-03-27 02:53:15 +0300

    train of chahges
    кой-что в корне изменил, когда наконец смог посмотреть скринкаст >_< и то не весь, увы.

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,7 +1,7 @@
 class RailwayStation < ApplicationRecord
   has_many :rel_railway_stations_routes
   has_many :routes, through: :rel_railway_stations_routes
-  has_many :trains
+  has_many :trains, foreign_key: :current_station_id
 
   validates :title, presence: true, length: { minimum: 2, maximum: 20 }
 end
