3064ce903fd1e58414955466cef3eafafa703495
CaptainPhilipp <Ph-s@mail.ru>
2017-03-28 01:11:01 +0300

    carriage seats counts

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -3,6 +3,7 @@ class Carriage < ApplicationRecord
 
   scope :coupe,   -> { where carriage_type: 'coupe' }
   scope :economy, -> { where carriage_type: 'economy' }
+  scope :seats_count, ->(position) { sum("#{position}_seats") }
 
-  TYPES = %w(economy coupe)  
+  TYPES = %w(economy coupe)
 end
