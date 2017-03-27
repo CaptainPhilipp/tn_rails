90e2228fb7a536b99fbeab417e09197217f86df2
CaptainPhilipp <Ph-s@mail.ru>
2017-03-28 01:50:49 +0300

    1 scope to 2 obvious scopes

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -3,7 +3,8 @@ class Carriage < ApplicationRecord
 
   scope :coupe,   -> { where carriage_type: 'coupe' }
   scope :economy, -> { where carriage_type: 'economy' }
-  scope :seats_count, ->(position) { sum("#{position}_seats") }
+  scope :bottom_seats, ->(position) { sum(:top_seats) }
+  scope :top_seats,    ->(position) { sum(:bottom_seats) }
 
   TYPES = %w(economy coupe)
 end
