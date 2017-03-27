aae4955fb56e28f560a5b78fb6c90fe48342cf2c
CaptainPhilipp <Ph-s@mail.ru>
2017-03-28 00:25:15 +0300

    economy/second-class carriages for train

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,5 +1,8 @@
 class Carriage < ApplicationRecord
   belongs_to :train
 
-  TYPES = %w(economy coupe)
+  scope :coupe,   -> { where carriage_type: 'coupe' }
+  scope :economy, -> { where carriage_type: 'economy' }
+
+  TYPES = %w(economy coupe)  
 end
