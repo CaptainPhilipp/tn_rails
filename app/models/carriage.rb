f2da85c990b0292ca4da9704c5d7d9e475bb89b2
CaptainPhilipp <Ph-s@mail.ru>
2017-03-30 00:06:02 +0300

    Major fixes

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -7,12 +7,10 @@ class Carriage < ApplicationRecord
   CarType = Struct.new(:type, :ru_title)
 
   # структы для вызова элементов хелперами форм, как методы
-  TYPES = [CarType.new('economy', 'Плацкарт'),
-           CarType.new('coupe', 'Купе'),
-           CarType.new('first_class', 'СВ'),
-           CarType.new('seat', 'Сидячий')].freeze
-
-  TYPES_LIST = TYPES.map { |car_type| "#{car_type.type.classify}Carriage" }.freeze
+  TYPES = [CarType.new('EconomyCarriage', 'Плацкарт'),
+           CarType.new('CoupeCarriage',   'Купе'),
+           CarType.new('FirstClassCarriage', 'СВ'),
+           CarType.new('SeatCarriage',    'Сидячий')].freeze
 
   def places_total
     top_places + bottom_places + side_top_places + side_bottom_places
