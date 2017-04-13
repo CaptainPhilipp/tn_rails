30c73ca301c2d3ed1ea976b122a3134fda6227f5
CaptainPhilipp <Ph-s@mail.ru>
2017-04-13 21:13:00 +0300

    admin base

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,3 +1,10 @@
 class Admin::Base < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :check_admin
+
+  protected
+
+  def check_admin
+    redirect_to root_path, alert: 'Go away, bastard!' unless current_user.admin?
+  end
 end
