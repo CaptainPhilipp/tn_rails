995d9bf4aa3c52ad43c29854e274a8fb6838cb0b
CaptainPhilipp <Ph-s@mail.ru>
2017-04-15 20:55:10 +0300

    bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  def i18n_submit(instance)
-    model_name = I18n.t(instance.model_name.singular, scope: 'activerecord.models')
+  def i18n_submit(instance, custom_action_name = nil)
+    model_name = I18n.t(instance.model_name.singular, scope: 'activerecord.models', count: 1)
     action_name = instance.new_record? ? t(:create) : t(:update)
-    "#{action_name} #{model_name.downcase}"
+    "#{custom_action_name || action_name} #{model_name.downcase}"
+  end
+
+  def glyphicon(name)
+    raw "<span class='glyphicon glyphicon-#{name}' aria-hidden='true'></span>"
   end
 end
