c8ed3a312de45f662b05d6a3901cd09aad4e1967
CaptainPhilipp <Ph-s@mail.ru>
2017-04-15 23:17:47 +0300

    scope locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   def after_sign_in_path_for(_resource)
     current_user.admin? ? admin_path : new_search_path
+  end
+
+  private
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || extract_locale_from_header || I18n.default_locale
+  end
+
+  def extract_locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).try(:first)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,33 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :tickets
-  end
-  devise_for :users
+  scope '/:locale', locale: /en|ru/ do
+    namespace :admin do
+      resources :tickets
+    end
+    devise_for :users
 
-  namespace :admin do
-    resources :routes, :carriages
+    namespace :admin do
+      resources :routes, :carriages
 
-    resources :trains do
-      resources :carriages
+      resources :trains do
+        resources :carriages
+      end
+
+      resources :railway_stations do
+        post :change_position, on: :member
+      end
+
+      get '', controller: :base, action: :index
     end
 
-    resources :railway_stations do
-      post :change_position, on: :member
+    resources :tickets, only: %i(index create show destroy) do
+      get 'new/:train_id/:departure_id/:arrival_id', to: 'tickets#new', on: :collection, as: :new
     end
 
-    get '', controller: :base, action: :index
+    resources :search, only: %i(new index) do
+      post :new, action: :find, on: :new
+    end
+    root 'static_pages#home'
   end
 
-  resources :tickets, only: %i(index create show destroy) do
-    get 'new/:train_id/:departure_id/:arrival_id', to: 'tickets#new', on: :collection, as: :new
-  end
-
-  resources :search, only: %i(new index) do
-    post :new, action: :find, on: :new
-  end
-
-  root 'static_pages#home'
+  get '/', to: 'static_pages#home'
 end
