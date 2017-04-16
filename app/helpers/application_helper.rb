7b86671eda944fbf5cbca9ff912f93cc9357fae3
CaptainPhilipp <Ph-s@mail.ru>
2017-04-16 17:20:29 +0300

    form layout helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,4 @@
 module ApplicationHelper
-  def i18n_submit(instance, custom_action_name = nil)
-    model_name = I18n.t(instance.model_name.singular, scope: 'activerecord.models', count: 1)
-    action_name = instance.new_record? ? t(:create) : t(:update)
-    "#{custom_action_name || action_name} #{model_name.downcase}"
-  end
-
   def glyphicon(name)
     raw "<span class='glyphicon glyphicon-#{name}' aria-hidden='true'></span>"
   end
