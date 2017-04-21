b1d5e817e0589fd1c5f4c1af042604ac6d558f09
CaptainPhilipp <Ph-s@mail.ru>
2017-04-21 20:38:23 +0300

    fix js changing form by carriage type

--- app/assets/javascripts/admin/new_carriage.js
+++ app/assets/javascripts/admin/new_carriage.js
@@ -1,16 +1,14 @@
 $(document).on("turbolinks:load", function() {
-  $('#carriage_type').change(function() {
-    console.log('change');
+  var collectionSelect = $('#carriage_type')
+
+  changeFieldset = function() {
     var field_groups = $('.carriage_type')
     field_groups.hide();
     field_groups.find('input:text').val(0);
-
-    var type = ""
-    if(type) {
-      type = $(this).find('option:selected').val();
-    } else {
-      type = $('#current_type').text();
-    }
+    type = collectionSelect.find('option:selected').val();
     $("#" + type).show();
-  });
+  };
+
+  changeFieldset();
+  collectionSelect.change(function() { changeFieldset() });
 });
